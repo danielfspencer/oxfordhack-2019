270f43b975da0b417c9c6999ebcb12f1e38e3983
Red <aryamanreddi@gmail.com>
2019-11-16 19:49:32 +0000

    Face detection & cropping

---
+++ facecrop.py
@@ -0,0 +1,23 @@
+import asyncio, io, glob, os, sys, time, uuid, requests
+from urllib.parse import urlparse
+from io import BytesIO
+from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
+
+# Set the FACE_SUBSCRIPTION_KEY environment variable with your key as the value.
+# This key will serve all examples in this document.
+KEY = os.environ['COGNITIVE_SERVICE_KEY']
+
+# Set the FACE_ENDPOINT environment variable with the endpoint from your Face service in Azure.
+# This endpoint will be used in all examples in this quickstart.
+ENDPOINT = os.environ['FACE_ENDPOINT']
+
+# Create an authenticated FaceClient.
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+print('Client Loaded')
+
+#%%
+
