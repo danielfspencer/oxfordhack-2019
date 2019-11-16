6f51e07b5d5c5469baeda466b7416540be5872a3
Daniel Spencer <danielfspencer1@gmail.com>
2019-11-16 20:48:01 +0000

    Add font compiler

---
+++ font/compose/nototools/py23.py
@@ -0,0 +1,15 @@
+
+try:
+    unicode = unicode
+except NameError:
+    unicode = str
+
+try:
+    unichr = unichr
+except NameError:
+    unichr = chr
+
+try:
+  basestring = basestring
+except NameError:
+  basestring = str
