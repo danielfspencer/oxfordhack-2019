e9ce415b61987003158b5b0968a3e0c49cb2d2e0
Daniel Spencer <danielfspencer1@gmail.com>
2019-11-17 11:06:03 +0000

    Cut out faces

--- server/creator.py
+++ server/creator.py
@@ -6,9 +6,11 @@ from translator import emojis
 from translator.translator import Face
 
 from font import compose
-
+from separator import separator
 
 def create(image_directory):
+    separator.init()
+
     faces = []
     for image in os.listdir(image_directory)[:10]:
         if not (image.endswith(".png") or image.endswith(".jpg")):
@@ -25,15 +27,33 @@ def create(image_directory):
 
     results = []
 
+    used_faces = []
+    with open("master_emoji_list", "r") as f:
+        for emoji in emojis.parse_lines(f):
+            result = model.emoji_to_face(emoji)
+            if result and not result in used_faces:
+                result.cropped.save(result.filename)
+                bg_removed = result.filename + ".bg_rem.png"
+                cropped = result.filename + ".crop.png"
+
+                separator.seperate(result.filename, bg_removed)
+
+                img = Image.open(bg_removed)
+                cropped_img = ImageOps.fit(img, (128, 128), method=Image.ANTIALIAS)
+                cropped_img.save(cropped)
+
+                result.filename = cropped
+
+                used_faces.append(result)
+
+
     with open("master_emoji_list", "r") as f:
         for emoji in emojis.parse_lines(f):
             result = model.emoji_to_face(emoji)
             if not result:
                 continue
 
-            cropped_location = result.filename + ".smaller.png"
-            result.cropped.save(cropped_location)
-            results.append((emoji.codepoint.lower(), cropped_location))
+            results.append((emoji.codepoint.lower(), result.filename))
 
     serve_dir = os.path.join(image_directory, "serve")
     shutil.copytree("font/tester/", serve_dir)
