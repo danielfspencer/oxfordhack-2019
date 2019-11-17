633beb00f432104621b62619eeda0c6e8915c405
Daniel Spencer <danielfspencer1@gmail.com>
2019-11-17 09:22:12 +0000

    Add API

---
+++ separator/separator/README.md
@@ -0,0 +1,13 @@
+#### Requires Python 3.6 or above
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+#### To test
+```bash
+python3 test.py
+```
+
+For API, see `test.py`
--- separator/separator/prototype.py
+++ separator/separator/prototype.py
@@ -9,6 +9,11 @@ import cv2
 # Apply the transformations needed
 import torchvision.transforms as T
 
+def init():
+    global dlab
+    print("loading dnn...")
+    dlab = models.segmentation.deeplabv3_resnet101(pretrained=1).eval()
+
 # Define the helper function
 def decode_segmap(image, source, nc=21):
 
@@ -23,58 +28,37 @@ def decode_segmap(image, source, nc=21):
                (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
 
   r = np.zeros_like(image).astype(np.uint8)
-  g = np.zeros_like(image).astype(np.uint8)
-  b = np.zeros_like(image).astype(np.uint8)
-
 
   for l in range(0, nc):
     idx = image == l
     r[idx] = label_colors[l, 0]
-    g[idx] = label_colors[l, 1]
-    b[idx] = label_colors[l, 2]
 
-  rgb = np.stack([r, g, b], axis=2)
+  classification = np.stack([r], axis=2)
 
   # Load the foreground input image
   foreground = cv2.imread(source)
 
   # Change the color of foreground image to RGB
   # and resize image to match shape of R-band in RGB output map
-  foreground = cv2.cvtColor(foreground, cv2.COLOR_BGR2RGB)
+  foreground = cv2.cvtColor(foreground, cv2.COLOR_BGR2RGBA)
   foreground = cv2.resize(foreground,(r.shape[1],r.shape[0]))
-
-  # Create a background array to hold white pixels
-  # with the same size as RGB output map
-  background = 255 * np.ones_like(rgb).astype(np.uint8)
 
   # Convert uint8 to float
   foreground = foreground.astype(float)
-  background = background.astype(float)
 
   # Create a binary mask of the RGB output map using the threshold value 0
-  th, alpha = cv2.threshold(np.array(rgb),0,255, cv2.THRESH_BINARY)
+  th, alpha = cv2.threshold(np.array(classification),0,255, cv2.THRESH_BINARY)
 
   # Apply a slight blur to the mask to soften edges
   alpha = cv2.GaussianBlur(alpha, (7,7),0)
 
-  # Normalize the alpha mask to keep intensity between 0 and 1
-  alpha = alpha.astype(float)/255
-
-  # Multiply the foreground with the alpha matte
-  foreground = cv2.multiply(alpha, foreground)
-
-  # Multiply the background with ( 1 - alpha )
-  background = cv2.multiply(1.0 - alpha, background)
-
-  # Add the masked foreground and background
-  outImage = cv2.add(foreground, background)
+  foreground[:,:,3] = alpha
 
   # Return a normalized output image for display
-  return outImage/255
+  return foreground/255
 
-def segment(net, path, show_orig=True, dev='cpu'):
-  img = Image.open(path)
-  if show_orig: plt.imshow(img); plt.axis('off'); plt.show()
+def segment(net, inpath, outpath, dev='cpu'):
+  img = Image.open(inpath)
   # Comment the Resize and CenterCrop for better inference results
   trf = T.Compose([T.Resize(450),
                    #T.CenterCrop(224),
@@ -85,14 +69,10 @@ def segment(net, path, show_orig=True, dev='cpu'):
   out = net.to(dev)(inp)['out']
   om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
 
-  rgb = decode_segmap(om, path)
+  rgb = decode_segmap(om, inpath)
 
-  # Image.fromarray(rgb).convert("RGB").save("out.png")
+  matplotlib.image.imsave(outpath, rgb)
 
-  matplotlib.image.imsave('out.png', rgb)
-
-  # plt.imshow(rgb); plt.axis('off'); plt.show()
-
-dlab = models.segmentation.deeplabv3_resnet101(pretrained=1).eval()
-
-segment(dlab, 'test.jpg', show_orig=False)
+def seperate(inpath, outpath):
+    print(f"seperate image '{inpath}'...")
+    segment(dlab, inpath, outpath)
