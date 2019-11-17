2db2c83239a7bac8fb3b574e69b63e23e0452469
Will Russell <jetpowerss@gmail.com>
2019-11-17 02:52:01 +0000

    Resize pictures to 128x128 pixels

---
+++ resize.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageOps
+
+def resize(file):
+    im = Image.open(file)
+    SIZE = 128, 128
+    im = ImageOps.fit(im, SIZE, method=Image.ANTIALIAS)
+    im.save(file)
+
+resize("pictures/picture1.jpg")
